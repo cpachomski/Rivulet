732803cbe716138e563500700a2616f4e919b648
Chris <cpachomski@gmail.com>
2016-07-13 14:52:17 -0400

    create searchbar component and emit searchTerm from state to update twitter stream

---
+++ client/components/Searchbar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Socket from '../../socket';
+
+export default React.createClass({
+
+	getInitialState() {
+		return {
+			searchTerm: ''
+		}
+	},
+
+	updateSearchTerm(e) {
+		console.log(e.target.value);
+
+		this.setState({
+			searchTerm: e.target.value
+		});
+	},
+
+	handleSearch(e) {
+		e.preventDefault();
+		Socket.emit('getNewStream', {searchTerm: this.state.searchTerm})
+	},
+
+	render() {
+		return (
+			<div className='searchbar'>
+				<form
+					onSubmit={this.handleSearch}>
+					<input
+						className='searchbar--input'
+						type='text'
+						onKeyUp={this.updateSearchTerm} />
+					<input 
+						className='searchbar--submit'
+						type='submit' />
+				</form>
+			</div>
+		)
+	}
+})
