1f38fbc431ec2fa44ddba9c83006091437508b34
Chris <cpachomski@gmail.com>
2016-07-13 13:25:56 -0400

    get socket and twitter stream to emit data to client

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
+var config = require('./config.js');
+var socketIO = require('socket.io');
+var Twit = require('twit');
 
 // create HTTP server with express application
 var server = require('http').createServer(app);
 var PORT = 3000;
-
-// create new websocket instance for the server
-var io = require('socket.io')(server);
 
 // serve public folder as static assets
 app.use(express.static( __dirname + '/public' ));
@@ -14,6 +14,27 @@ app.use(express.static( __dirname + '/public' ));
 app.get('/', function(req, res, next) {
 	res.sendFile( __diirname + '/public/index.html' )
 });
+
+// create new websocket instance for the server
+var io = socketIO.listen(server);
+
+// connect to twitter with creds
+var TCon = new Twit({
+	consumer_key: config.twitter.consumerKey,
+	consumer_secret: config.twitter.consumerSecret,
+	access_token: config.twitter.accessToken,
+	access_token_secret: config.twitter.accessTokenSecret
+});
+
+var stream = TCon.stream('statuses/sample');
+
+io.sockets.on('connection', function(socket) {
+	stream.on('tweet', function(tweet) { 
+		console.log('test');
+		socket.emit('payload', { tweet: tweet })
+	});
+});
+
 
 server.listen(PORT);
 console.log('Serving up some good stuff at localhost:' + PORT + '...');
